ca8c3f31dd6e7e58feaaf825fff680aa22deaafc
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-08 06:50:52 -0300

    modificando de header para navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 import { CardsComponent } from './cards/cards.component';
@@ -13,7 +13,7 @@ import { RepositoriosComponent } from './repositorios/repositorios.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavbarComponent,
     SidebarComponent,
     HomeComponent,
     CardsComponent,
